a3e24fcfc0acf003aa2d27451f43bdb9184f55d7
theryanclausen <theryanclausen@gmail.com>
2019-03-05 01:59:53 -0700

    space

--- portfolio/src/components/Splash.js
+++ portfolio/src/components/Splash.js
@@ -1,77 +1,117 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
-import { Link } from 'react-scroll'
-import logo from "../assets/logo.png";
+import { Link } from "react-scroll";
 
-const AppLogoSpin = keyframes`
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  `;
+const randomInt = (min, max) => Math.random() * (max - min + 1) + min
+
+const glow = keyframes`
+0% {
+    box-shadow: inherit;
+    transform: rotate(0deg);
+  }
+  19% {
+    opacity: 1;
+  }
+  20% {
+    opacity: .7;
+  }
+  21% {
+    opacity: 1;
+  }
+  50% {
+    box-shadow: 0px 1px 8px 5px #f9eaea4d;
+  }
+  100% {
+    transform: rotate(360deg);
+    box-shadow: 0px 0px 5px 1px #f9eaea;
+  }
+`
+
+const shoot = keyframes`
+0% {
+  transform: scale(0) translate3d(0, 0, 0);
+  opacity: 0;
+}
+25% {
+  opacity: 1;
+}
+35% {
+  transform: scale(1) translate3d(-800px, 800px, 0);
+  opacity: 0;
+}
+100% {
+  transform: translate3d(0px, 0px, 0);
+  opacity: 0;
+}
+`
+
 
 const StyledSplash = styled.div`
   text-align: center;
-  background-color: #000;
+  background: radial-gradient(
+    ellipse at bottom,
+    #330a2d 0%,
+    #060923 24%,
+    #08000c 55%,
+    #000000 100%
+  );
   min-height: 105vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  font-size: 28px ;
+  font-size: 28px;
   color: white;
   header {
-    position:relative;
+    position: relative;
     width: 400px;
-    .over-top{
-      position:absolute;
-      width:100%;
-      top: 70px;
-      padding:4px;
-      z-index:10;
-      font-size:100px;
-      font-family: 'Cabin Sketch', cursive;
-      fill:red ;
-      p{
-        font-size:154px;
-        letter-spacing:10px;
-      }
+    h1{
+      font-size: 66px;
+      margin: 65px 0;
     }
-    .react-boiler {
-      font-family:"Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-      overflow:hidden;
-      width:400px;
-      line-height:1.4;
-      .App-logo {
-        animation: ${AppLogoSpin} infinite 20s linear;
-        height: 400px;
-        border-radius:50%;
-        pointer-events: none;
-      }
-      .App-link {
-        color: #61dafb;
-        cursor: pointer;
-      }
+    h2{
+      font-size:44px;
+      margin: 25px 0 44px;
     }
   }
 `;
 
+const Star = styled.div`
+  position: absolute;
+  left: ${()=>randomInt(1,100)}%; 
+  top: ${()=>randomInt(1,100)}%;
+  border-radius: 50%;
+  box-shadow: ${()=>randomInt(-1,1)}px 0px ${()=>randomInt(18,34)}px 6px #edeeff;
+  background:#fffbed;
+  width:${props => `${props.size}px`};
+  height:${props => `${props.size}px`};
+animation: ${glow} ${()=>randomInt(5,11)}s ease-in-out infinite;
+`
+
+const ShootingStar = styled.div`
+  width: 4px;
+  height: 4px;
+  position: absolute;
+  border-radius: 50%;
+  background: rgba(255, 205, 205, 1);
+  right: ${()=>randomInt(1,60)}%;
+  top: ${()=>randomInt(1,40)}%;
+  animation: ${shoot} ${()=>randomInt(5,9)}s ease-in-out infinite;
+  animation-delay: ${()=>randomInt(1,82)}s;
+  transform: translate3d(0, 0, 0);
+  opacity: 0;
+`
+
 export default () => {
   return (
-    <StyledSplash id='splash'>
+    <StyledSplash id="splash">
+    {new Array(199).fill('').map((x,i) => <Star key={i} size={randomInt(1,4)} />)}
+    {new Array(6).fill('').map((x,i)=> <ShootingStar key={i}></ShootingStar>)}
       <header>
-        <div className='over-top'>
-          <p>Ryan</p>Clausen
-        </div>
-        <div className="react-boiler">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Full Stack Web Developer
-          </p>
-          <Link to="about" smooth={true} duration={1000}className="App-link">Learn More</Link>
-        </div>
+        <h1>Ryan Clausen</h1><h2>Fullstack Developer</h2>
+        <Link to="about" smooth={true} duration={1000} className="App-link">
+          Learn More
+        </Link>
       </header>
     </StyledSplash>
   );
